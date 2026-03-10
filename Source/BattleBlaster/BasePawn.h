cc7d1c59b0b2e70ba16887bf437b79865e61c2c6
Chiraag Rokade <94799807+ChiraagRokade@users.noreply.github.com>
2026-03-10 16:56:28 +0530

    Create Base Pawn class

--- Source/BattleBlaster/BasePawn.h
+++ Source/BattleBlaster/BasePawn.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "Components/CapsuleComponent.h"
 #include "BasePawn.generated.h"
 
 UCLASS()
@@ -26,4 +27,12 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UCapsuleComponent* CapsuleComp;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* BaseMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* TurretMesh;
 };
